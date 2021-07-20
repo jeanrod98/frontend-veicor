9862de9544588aa7ec05969967a0b425011b0a39
jeanrod98 <jcrod.jc@gmail.com>
2021-07-20 16:58:47 -0500

    modales de terminos y pago terminados faltan validacion

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link, withRouter } from "react-router-dom";
+import {useState} from 'react';
 import "../css/carrito.css";
+import logo from '../assets/logos/logopng2.png'
+import {Modal, Button, Form, FormControl, InputGroup, FormGroup} from 'react-bootstrap';
 
 function Carrito(props) {
+
   const total_inpu = 5 * 0.12;
 
   const detalles = {
@@ -11,10 +15,12 @@ function Carrito(props) {
     total: total_inpu,
   };
 
-  const irPagar = e =>{
-    props.history.push('/transaccion');
-    
-  }
+  
+// Estado para el modal de transaccion 
+  const [openModalTransaccion, setOpenModalTransaccion] = useState(false)
+
+  // estado para el modal de terminos 
+  const [openModalTerminos, setOpenModalTerminos] = useState(false)
 
   return (
     <div className="Carrito">
@@ -31,7 +37,21 @@ function Carrito(props) {
         <div className="carrito-productos">
           {/* Detalle de los Productos  */}
           <div className="carrito-detalle">
-            <h6>Detalle de los productos</h6>
+            <div className="detalle-productos">
+
+              <div className="productos-noexisten">
+
+                    <h1>El carrito está vacío</h1>
+                    <div className="text-center">
+
+                      <ion-icon name="alert-circle-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+
+            <div className="detalle-btnVaciar">
+              <button className="btn vaciar-carrito">VACIAR CARRITO</button>
+            </div>
             
           </div>
 
@@ -100,8 +120,7 @@ function Carrito(props) {
                 <label class="form-check-label" for="exampleCheck1">
                   Acepto los términos y condiciones{" "}
                   <a
-                    data-toggle="modal"
-                    data-target="#exampleModalLong"
+                    onClick={(() => setOpenModalTerminos(true))}
                     href="#"
                   >
                     Ver.
@@ -113,7 +132,7 @@ function Carrito(props) {
             <div className="carrito-botones">
               {/* Boton para ingresar metodo de pago  */}
               <div className="btn-1">
-                <button className="btn finalizar-pago" onClick={irPagar}>FINALIZAR COMPRA</button>
+                <button className="btn finalizar-pago" onClick={(() =>{setOpenModalTransaccion(true)})}>FINALIZAR COMPRA</button>
               </div>
 
               {/* enlace a catalogo  */}
@@ -127,50 +146,87 @@ function Carrito(props) {
         </div>
       </div>
 
-      {/* <!-- Modal Terminos y Condiciones  --> */}
-      <div
-        class="modal fade"
-        id="exampleModalLong"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLongTitle"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">
-                Términos y Condiciones
-              </h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur, voluptatum qui quisquam sit velit id dolorem magnam
-              asperiores eos recusandae obcaecati quos vitae, consequatur
-              reiciendis consectetur aliquam tempora ipsam minima! Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Autem officiis nisi
-              laudantium quod deleniti sequi commodi libero ad perspiciatis
-              velit? Nesciunt voluptatum nemo sit officia tenetur vero
-              reiciendis officiis eveniet!
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Cerrar
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+     
+      {/* Modal de Terminos y Condiciones  */}
+      <Modal show={openModalTerminos}>
+        <Modal.Header>
+          <h4>Terminos y Condiciones</h4>
+        </Modal.Header>
+
+        <Modal.Body>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur, voluptatum qui quisquam sit velit id dolorem magnam
+            asperiores eos recusandae obcaecati quos vitae, consequatur
+            reiciendis consectetur aliquam tempora ipsam minima! Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Autem officiis nisi
+            laudantium quod deleniti sequi commodi libero ad perspiciatis
+            velit? Nesciunt voluptatum nemo sit officia tenetur vero
+            reiciendis officiis eveniet!
+        </Modal.Body>
+        <Modal.Footer>
+          
+          <Button 
+          onClick={(() =>{setOpenModalTerminos(false)})}
+          className="btn-secondary"
+          >
+            Cerrar
+          </Button>
+           
+        </Modal.Footer>
+      </Modal>
+      {/* Modal de Transaccion  */}
+      <Modal 
+  
+      centered
+      show={openModalTransaccion}>
+        <Modal.Header >
+          <Modal.Title className='modal-title-pago'>
+            <img src={logo} width='80px' height='65px'/>           
+            <h4>TRANSACCIÓN DE PAGO</h4>
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            {/* Correo  */}
+            <InputGroup>
+              <InputGroup.Text><ion-icon name="mail-outline"></ion-icon></InputGroup.Text>
+              <FormControl isValid={false} isInvalid={false} id="modalCorreo" placeholder="Correo electrónico" />
+            </InputGroup>
+            {/* Tarjeta  */}
+            <FormGroup className="modal-form-group">
+              <InputGroup>
+                <InputGroup.Text><ion-icon name="card-outline"></ion-icon></InputGroup.Text>
+                <FormControl isValid={false} isInvalid={false} id="modalNumTarjeta" placeholder="Número de Tarjeta" />
+              </InputGroup>
+              <div className="seccion-modal-tarjeta-datos">
+                <InputGroup>
+                  <InputGroup.Text><ion-icon name="calendar-clear-outline"></ion-icon></InputGroup.Text>
+                  <FormControl isValid={false} isInvalid={false} id="modalMesAnioTarjeta" placeholder="MM/YY" />
+                </InputGroup>
+
+                <InputGroup inline>
+                  <InputGroup.Text><ion-icon name="lock-closed-outline"></ion-icon></InputGroup.Text>
+                  <FormControl isValid={false} isInvalid={false} id="modalCvcTarjeta" placeholder="CVC" />
+                </InputGroup>
+
+              </div>
+            </FormGroup>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="btn-modal-pago" >
+            Realizar Pago
+          </Button>
+          <Button 
+          onClick={(() =>{setOpenModalTransaccion(false)})}
+          variant="secondary"
+          >
+            Cerrar
+          </Button>
+           
+        </Modal.Footer>
+      </Modal>
 
       
 
